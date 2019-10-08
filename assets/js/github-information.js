162d357eca6043c050304723e13848d70faba0bb
Damien Meere <damien.meere@bt.com>
2019-10-08 15:36:42 +0000

    Troubleshooting on github interface compaletd

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,28 +1,25 @@
-function userInformationHTML(){
+function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
-                (@<a href="${user.html_url}" target="_blank">${user.login}</a>
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
-            <div class="gh-avatar>
-                <a href="${user.html_url} target="_blank">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            <p>Followers:  ${user.followers} - Following: ${users.following} <br> Repo: ${users.public_repos} </p>
-
-        </div>`
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
-
-function fetchGitHubInformation(event){
+function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
-    // if no username is supplied, return statement
-    if(!username){
-        $("#gh-user-data").html('<h2>Please Enter a GitHub username</h2>');
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
 
@@ -34,20 +31,18 @@ function fetchGitHubInformation(event){
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
-        function(response){
+        function(response) {
             var userData = response;
             $("#gh-user-data").html(userInformationHTML(userData));
-        }, function(errorResponse){
-            if (errorReponse.status === 404){
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
-
-                    `<h2>No Info Found For User ${username}</h2>`);
-            }else {
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2> Error: ${errorResponse.responseJSON.message}</h2>`
-                );
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-        }
-    )
+        });
 }
