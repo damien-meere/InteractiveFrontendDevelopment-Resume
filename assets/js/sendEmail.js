8dc6edd6952e0febd3709c190e04a265bf413392
Damien Meere <damien.meere@bt.com>
2019-10-16 15:11:20 +0000

    linked the contact form to emailjs API

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendEmail(contactForm) {
+
+
+    emailjs.send("outlook", "code_institute_resume", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+
+}
+
+
+
